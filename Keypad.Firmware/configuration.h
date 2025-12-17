974cf5d796da0828890b192db05414fa9f60a838
Amy Jeanes <amyjeanes23@gmail.com>
2025-12-17 01:27:52 +0000

    exclude default config from docker build

---
+++ Keypad.Firmware/configuration.h
@@ -0,0 +1,9 @@
+// This file is replaced at runtime by the configuration generator tool in the backend
+// It exists only to test the compilation of the firmware with a sample configuration
+
+#pragma once
+
+#include "src/configuration_data.h"
+
+#define CONFIGURATION_BUTTON_CAPACITY 4
+#define CONFIGURATION_ENCODER_CAPACITY 1
